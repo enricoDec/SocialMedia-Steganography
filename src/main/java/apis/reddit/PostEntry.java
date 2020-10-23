40c0910d7b13cb4f51e07c5a32ea99c2fb0ed74d
Teklic, Mario (init) <mario.teklic@init.de>
2020-10-23 17:09:30 +0200

    Postentry comparable

---
+++ src/main/java/apis/reddit/PostEntry.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2020
+ * Contributed by NAME HERE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package apis.reddit;
+/*
+ *
+ * @author Mario Teklic
+ */
+
+/**
+ * Represents a post from reddit
+ *
+ */
+public class PostEntry implements Comparable{
+
+    /**
+     * A downloadable URL for a media i.e. an image
+     */
+    private String url;
+
+    /**
+     * The timestamp, when the post was made as a long value
+     * There is no setter because the timestamp should not be changeable
+     */
+    private final MyDate date;
+
+    public PostEntry(String url, MyDate date){
+        this.url = url;
+        this.date = date;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public MyDate getDate() {
+        return date;
+    }
+
+    /**
+     *
+     * @param o
+     * @return 1 if equal
+     * @return -1 if not equal
+     */
+    @Override
+    public int compareTo(Object o) {
+        PostEntry postEntry = (PostEntry) o;
+        if(this.url == postEntry.getUrl()){
+            return 1;
+        }
+        return -1;
+    }
+}
