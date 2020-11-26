b73ac5b6f959f899410ee04735c0c87a83a04083
mtek <mario.teklic77@googlemail.com>
2020-11-26 16:34:12 +0100

    merge fix

--- src/main/java/apis/SocialMedia.java
+++ src/main/java/apis/SocialMedia.java
@@ -41,9 +41,9 @@ public abstract class SocialMedia {
     }
 
 
-    abstract Token<?> getToken();
+    public abstract Token getToken();
 
-    abstract void setToken(Token<?> token);
+    public abstract void setToken(Token token);
 
     /**
      * Post media on this Social Media under the keyword
@@ -51,19 +51,19 @@ public abstract class SocialMedia {
      * @param keyword keyword to search this post by
      * @return true if successful
      */
-    abstract boolean postToSocialNetwork(byte[] media, String keyword);
+    public abstract boolean postToSocialNetwork(byte[] media, String keyword);
 
     /**
      * Subscribe to a keyword (Hashtag / Title / ...)
      * @param keyword keyword to subscribe to
      * @return true if successful
      */
-     abstract boolean subscribeToKeyword(String keyword);
+    public abstract boolean subscribeToKeyword(String keyword);
 
     /**
      * Get Medias posted under keyword
      * @param keyword hashtag
      * @return true if successful
      */
-    abstract List<byte[]> getRecentMediaForKeyword(String keyword);
+    public abstract List<byte[]> getRecentMediaForKeyword(String keyword);
 }
--- src/main/java/steganography/image/test.java
+++ src/main/java/steganography/image/test.java
@@ -28,6 +28,7 @@ import java.io.*;
 public class test {
 
     public static void main( String[] args ) {
+        /*
         String pathToImage = "../testFiles/mariot.png";
 
         String loremIpsum = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labor";
@@ -82,5 +83,7 @@ public class test {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        */
+        
     }
 }
