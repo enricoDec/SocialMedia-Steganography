872e44135d2bcac3942a7282778ab85de1d60df3
marfen <fenzl.mario94@gmail.com>
2020-12-16 15:09:11 +0100

    added Tumblr

---
+++ src/main/java/apis/tumblr/TumblrConstants.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2020
+ * Contributed by NAME HERE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package apis.tumblr;
+
+public interface TumblrConstants {
+
+    String apiKey = "OfpsSPZAf9mClIvgVAKY3Hhg63Y09riZ9AMmbbI0hQVMdS4uYR";
+    String apiSecret = "H2yGuhhwd7g6eXIYE0OHpkL7fEd9laDWPHArjipezGyq9dFheF";
+    String callback = "https://example.com";
+
+    //only for testing, in live mode get these from android app after authorization
+    String accessToken = "sk4dqsFfEScw9NzORZ5x9s7DasAsfhHlAZYhe2nbAsWmBF6SwU";
+    String accessTokenSecret = "3cCMy7v1fkOMuSqJUe1nriAzg78nsgtnBHRje0sxSwPbDQCRmm";
+
+}
