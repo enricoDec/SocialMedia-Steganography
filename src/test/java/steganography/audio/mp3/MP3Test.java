d7ca85bbd18f9fe8e9bb911c4ed3b4e7e341c247
PebblesFTW <r.rudek@web.de>
2020-12-17 14:21:28 +0100

    mp3 - fixed test

--- src/test/java/steganography/audio/mp3/MP3Test.java
+++ src/test/java/steganography/audio/mp3/MP3Test.java
@@ -5,11 +5,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import socialmediasteganography.SocialMediaSteganographyException;
 import steganography.Steganography;
+import steganography.audio.overlays.MP3Overlays;
 import steganography.util.ByteArrayUtils;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 
 public class MP3Test {
 
@@ -18,12 +20,13 @@ public class MP3Test {
 
     @BeforeEach
     public void setSteganography() {
-        this.mp3Steg = new MP3Steganography();
+        this.mp3Steg = new MP3Steganography(MP3Overlays.SEQUENCE_OVERLAY);
         this.pathToTestFile = "src/test/resources/steganography/audio/mp3/testFile.mp3";
     }
 
     @Test
-    public void encodeSimpleStringInMp3_ExpectSameStringAfterDecode() throws IOException, SocialMediaSteganographyException {
+    public void encodeSimpleStringIntoByteArray_ExpectSameStringAfterDecode()
+            throws IOException, SocialMediaSteganographyException {
         // read bytes
         byte[] mp3Bytes = ByteArrayUtils.read(new File(this.pathToTestFile));
 
@@ -37,6 +40,37 @@ public class MP3Test {
         // read message
         byte[] decodedMessageBytes = this.mp3Steg.decode(encodedBytes);
         String decodedMessage = new String(decodedMessageBytes, StandardCharsets.UTF_8);
+
+        // assert messages are the same
+        Assertions.assertEquals(message, decodedMessage);
+    }
+
+    @Test
+    public void encodeSimpleStringIntoFile_ExpectSameStringAfterDecode()
+            throws IOException, SocialMediaSteganographyException {
+        String pathToEncodedFile = "src/test/resources/steganography/audio/mp3/testFileEncoded.mp3";
+
+        // read bytes
+        byte[] mp3Bytes = ByteArrayUtils.read(new File(this.pathToTestFile));
+
+        // create message byte array
+        String message = "Hello World!";
+        byte[] messageBytes = message.getBytes(StandardCharsets.UTF_8);
+
+        // encode message
+        byte[] encodedBytes = this.mp3Steg.encode(mp3Bytes, messageBytes);
+
+        // to file
+        File testFileEncoded = new File(pathToEncodedFile);
+        Files.write(testFileEncoded.toPath(), encodedBytes);
+
+        // read bytes and delete file
+        byte[] encodedBytesFromFile = ByteArrayUtils.read(testFileEncoded);
+        testFileEncoded.delete();
+
+        // decode message
+        byte[] messageFromFile = this.mp3Steg.decode(encodedBytesFromFile);
+        String decodedMessage = new String(messageFromFile, StandardCharsets.UTF_8);
 
         // assert messages are the same
         Assertions.assertEquals(message, decodedMessage);
